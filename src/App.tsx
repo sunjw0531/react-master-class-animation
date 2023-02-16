96dafde339bd7e9cf2836518483ddf3ccc39d1f1
정승우 <sunjw0531@gmail.com>
2023-02-16 16:52:34 +0900

    Animations : Gestures 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -16,30 +17,39 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.34);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // overflow: hidden;
+`;
+
 const boxVariants = {
   hover: {
-    scale: 2,
     rotateZ: 90,
   },
-  click: { scale: 1, borderRadius: '100px' },
-  drag: {
-    backgroundColor: 'rgb(46,204,114)',
-    transition: {
-      duration: 10,
-    },
-  },
+  click: { borderRadius: '100px' },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
