017f8c3b82cdce51dda3970ba2bb5f38f30dfafa
정승우 <sunjw0531@gmail.com>
2023-02-17 23:09:08 +0900

    End Project of Animation

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,9 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   height: 200px;
+
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
   justify-content: center;
@@ -28,13 +29,14 @@ const Box = styled(motion.div)`
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   width: 50vw;
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
+  opacity: 0.5;
+  // div:first-child,
+  // div:last-child {
+  //   grid-column: span 2;
+  // }
 `;
 
 const Overlay = styled(motion.div)`
@@ -46,28 +48,96 @@ const Overlay = styled(motion.div)`
   align-items: center;
 `;
 
+const Circle = styled(motion.div)`
+  border-radius: 100px;
+  width: 50px;
+  height: 50px;
+  background: yellow;
+  z-index: 999;
+`;
+
+const Button = styled(motion.button)`
+  position: absolute;
+  bottom: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const boxVariants = {
+  hover: (direction: number) => ({
+    originY: direction === 1 ? 1 : 0,
+    originX: direction === 1 ? 1 : 0,
+    scale: 1.1,
+  }),
+};
+
+const buttonVariants = {
+  changeBtn: (clicked: boolean) => ({
+    scale: clicked ? 1.3 : 1,
+    color: clicked ? 'rgba(255,105,46,1)' : 'rgba(0,140,255,1)',
+    transition: {
+      duration: 0.5,
+    },
+  }),
+};
+
 function App() {
   const [id, setId] = useState<null | string>(null);
+  const [circle, setCircle] = useState(2);
+  const [direction, setDirection] = useState(1);
+  const [clicked, setClicked] = useState(false);
   return (
-    <Wrapper>
-      <Grid>
-        {[1, 2, 3, 4].map((n) => (
-          <Box onClick={() => setId(n + '')} key={n} layoutId={n + ''} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: 'rgba(0,0,0,0)' }}
-            animate={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
-            exit={{ backgroundColor: 'rgba(0,0,0,0)' }}
-          >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Grid>
+          {[1, 2, 3, 4].map((n) =>
+            n === 1 || n === 4 ? (
+              <Box
+                custom={direction}
+                variants={boxVariants}
+                whileHover="hover"
+                onMouseOver={() => setDirection(n)}
+                onClick={() => setId(n + '')}
+                key={n}
+                layoutId={n + ''}
+                transition={{ type: 'tween' }}
+              />
+            ) : (
+              <Box key={n}>
+                {circle === n ? <Circle layoutId="circleBox" /> : null}
+              </Box>
+            )
+          )}
+        </Grid>
+        <AnimatePresence>
+          {id ? (
+            <Overlay
+              onClick={() => setId(null)}
+              initial={{ backgroundColor: 'rgba(0,0,0,0)' }}
+              animate={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
+              exit={{ backgroundColor: 'rgba(0,0,0,0)' }}
+            >
+              <Box layoutId={id} style={{ width: 400, height: 200 }} />
+            </Overlay>
+          ) : null}
+        </AnimatePresence>
+      </Wrapper>
+      <Button
+        onClick={() => {
+          setCircle((prev) => {
+            const next = prev + 1;
+            if (next === 4) return 2;
+            return 3;
+          });
+          setClicked((prev) => !prev);
+        }}
+        variants={buttonVariants}
+        custom={clicked}
+        animate="changeBtn"
+      >
+        Switch
+      </Button>
+    </>
   );
 }
 
